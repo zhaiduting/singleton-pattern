9f1a6b4836455e7c8d0366ec9c60c6834838068c
zhaiduting <zhaiduting@163.com>
2022-09-08 04:00:14 +0800

    isset

--- tools/print_rn.php
+++ tools/print_rn.php
@@ -5,7 +5,7 @@ function print_rn($msg, $return = false)
     $msg = print_r($msg, true);
     if ($return)
         return $msg;
-    if ($_SERVER['HTTP_USER_AGENT'])
+    if (isset($_SERVER['HTTP_USER_AGENT']))
         $msg = "<pre>$msg</pre>";
     else
         $msg .= substr($msg, -1, 1) !== "\n" ? "\n" : '';
